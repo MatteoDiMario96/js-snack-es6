7579ffbd0bb9676d0881217193ac7be274cc5a50
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-23 16:28:02 +0200

    Creata variabile per poter cosi asseganre il map. Chiamare il map sull'array ospiti. Trasformando il tutto in 3 keys con i valori effetivi.

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,3 +1,11 @@
-const listaOspiti = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+const ospiti = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
+const listaSegnaPosto = ospiti.map((ospite, indice) => {
+    return {
+        nomeTavolo: "Tavolo Vip",
+        nomeOspite: ospite,
+        "posto-n": indice + 1
+    };
+});
+console.log(listaOspiti, ospiti);
 
