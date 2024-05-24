393c9d3288159faf0981963791722de49052c2d4
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-24 19:50:36 +0200

    Creato array Squadre di calcio.

---
+++ Snack 5/js/script.js
@@ -0,0 +1,42 @@
+const squadreDiCalcio = [
+{ 
+    nome: "Juventus", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Milan", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Inter", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Roma", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Napoli", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Lazio", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Torino", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+},
+{ 
+    nome: "Atalanta", 
+    puntiFatti: 0, 
+    falliSubiti: 0 
+}
+];
