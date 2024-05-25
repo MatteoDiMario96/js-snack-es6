b955e050fea6c276974c46802c106de3cebd8472
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-25 13:41:25 +0200

    Sbagliato ordine snack corretto.

--- Snack 4/js/script.js
+++ Snack 4/js/script.js
@@ -1,55 +1,62 @@
-const biciDaCorsa = [
-{ 
-    nome: "Bianchi Oltre XR4", 
-    peso: 7.2 
-},
-{ 
-    nome: "Specialized Tarmac SL7", 
-    peso: 6.8 
-},
-{ 
-    nome: "Cannondale SuperSix EVO", 
-    peso: 7.5 
-},
-{ 
-    nome: "Trek Emonda SLR", 
-    peso: 6.5 
-},
-{ 
-    nome: "Pinarello Dogma F12", 
-    peso: 7.3 
-},
-{ 
-    nome: "Scott Addict RC", 
-    peso: 7.1 
-},
-{ 
-    nome: "Colnago C64", 
-    peso: 7.8 
-},
-{ 
-    nome: "Giant TCR Advanced Pro", 
-    peso: 6.9 
-},
-{ 
-    nome: "Merida Reacto", 
-    peso: 7.0 
-},
-{ 
-    nome: "Orbea Orca", 
-    peso: 6.3 
-}
-];
-
-//Non mi piace per niente aver inizializzato la variabile con una bici a caso per poi confrontarla con tutte 
-let biciMinorPeso = biciDaCorsa[0];
-biciDaCorsa.forEach(bicicletta => {
-    if(bicicletta.peso < biciMinorPeso.peso){
-        biciMinorPeso = bicicletta;
+const squadreDiCalcio = [
+    { 
+        nome: "Juventus", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Milan", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Inter", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Roma", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Napoli", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Lazio", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Torino", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Atalanta", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
     }
-});
-
-const  {nome, peso} = biciMinorPeso;
-
-console.log(`La bici più leggera al mondo è: la ${nome} con i suoi ben ${peso} kg di peso. Un mostro per il suo genere.`);
-console.log(biciMinorPeso);
+    ];
+    
+    function getRandomNumber (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    
+    let {nome, falliSubiti} = squadreDiCalcio;
+    
+    
+    squadreDiCalcio.forEach(squadra => {
+        squadra.puntiFatti = getRandomNumber(30, 100);
+        squadra.falliSubiti = getRandomNumber(5, 15);
+    });
+    
+    console.log(squadreDiCalcio);
+    
+    const nuovaListaSquadre = squadreDiCalcio.map(({nome, falliSubiti}) => ({nome, falliSubiti}));
+    
+    console.log(nuovaListaSquadre);
+    
+    
