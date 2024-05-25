3731159152ad9044c7fe770f50f5d5976818f7c4
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-25 17:54:19 +0200

    Aggiunte due funzioni. Prima per creare lettera unica. Seconda per completezza e crearne quante ne voglio. Create 6 lettere uniche.

--- Snack 5/js/script.js
+++ Snack 5/js/script.js
@@ -20,18 +20,41 @@ const articoliModa = [
         color: "pink"
     }
 ];
-
-const position = generaLettere(1);
-console.log(position);
+const caratteriDaVedere = [];
+const caratteriEstratti = serieDicaratteriUnici(6, 1, caratteriDaVedere);
+console.log(caratteriEstratti);
 
 
 //**************FUNZIONI**************//
 function generaLettere(numeroCaratteri) {
     const caratteri ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let risultato = ' ';
+    let risultato = '';
     for ( let i = 0; i < numeroCaratteri; i++ ) {
         risultato += caratteri.charAt(Math.floor(Math.random() * caratteri.length));
     }
 
     return risultato;
+};
+
+function generaUnicaLettera(numeroCaratteri, caratteriGiaEstratti){
+    let lettereCasuali;
+    let trovato = true;
+    while(trovato){
+        lettereCasuali = generaLettere(numeroCaratteri);
+        if(caratteriGiaEstratti.includes(lettereCasuali)){
+            trovato = true;
+        }else{
+            trovato = false;
+        }
+    };
+
+    return lettereCasuali
+}
+
+function serieDicaratteriUnici(numeroCaratteriUnici, numeroCaratteri, caratteriGiaEstratti){
+    caratteriGiaEstratti = [];
+    for(let i = 0; i < numeroCaratteriUnici; i++){
+    caratteriGiaEstratti.push(generaUnicaLettera(numeroCaratteri,caratteriGiaEstratti))
+}
+return caratteriGiaEstratti;
 }
